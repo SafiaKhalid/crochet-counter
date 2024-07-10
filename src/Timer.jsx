cd5ab5fb8e9f28f278888356194e4031cbdd4806
SafiaKhalid <safia.am.khalid@gmail.com>
2024-07-10 17:25:37 +0100

    Start, pause and reset timer button function set up

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Timer = () => {
-    return <div>timer</div>
+    const [time, setTime] = useState(0)
+
+    const startTimer = () => {
+        console.log('start');
+    }
+    
+    const pauseTimer = () => {
+        console.log('pause');
+    }
+    const resetTimer = () => {
+        console.log('reset');
+    }
+
+    return <section className="timer">timer: {time}
+        <button className="function-btn" onClick={startTimer}>start</button>
+        <button className="function-btn" onClick={pauseTimer}>pause</button>
+        <button className="function-btn" onClick={resetTimer}>reset</button>
+    </section>
 }
 
 export default Timer
