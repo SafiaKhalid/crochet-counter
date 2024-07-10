0bb4d26a8b9b54cdfae6dc4d00e5a3c0f7dcde22
SafiaKhalid <safia.am.khalid@gmail.com>
2024-07-10 17:20:23 +0100

    Created Timer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Counter from './Counter'
+import Timer from './Timer'
 
 function App() {
   const [stitches, setStitches] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <h1>Crochet Counter</h1>
       <Counter typeName='stitches' type={stitches} setType={setStitches}/>
       <Counter typeName='rows' type={rows} setType={setRows}/>
+      <Timer/>
     </>
   )
 }
