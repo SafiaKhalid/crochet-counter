f9448a189d11893293141d7be253ba834d2416bb
SafiaKhalid <safia.am.khalid@gmail.com>
2024-07-10 17:06:41 +0100

    Initialised value useState, counter button onClick function setup

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () => {
-    return <div>counter</div>
+    const [value, setValue] = useState(0)
+
+    const decrease = () => {
+        console.log('decrease')
+    }
+    
+    const increase = () => {
+        console.log('increase')
+    }
+    
+    const reset = () => {
+        console.log('reset')
+    }
+
+    return <section>counter: {value}
+        <button className="counter-btn" onClick={decrease}>-</button>
+        <button className="counter-btn" onClick={increase}>+</button>
+        <button className="reset-btn" onClick={reset}>reset</button>
+    </section>
 }
 
 export default Counter
